aedfa97e8d562a50d230985907966b2ef2737952
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-07-09 16:45:43 +0200

    refs

---
+++ refs/src/Counter.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "./Button";
+import { CounterDisplay } from "./CounterDisplay";
+
+export function Counter({ initialValue }) {
+  const [counter, setCounter] = useState(initialValue);
+  const directionRef = useRef("");
+  const prevCounterRef = useRef(initialValue);
+
+  useEffect(() => {
+    if (counter > prevCounterRef.current && directionRef.current !== "su") {
+      directionRef.current = "su";
+      console.log(directionRef.current);
+    } else if (counter < prevCounterRef.current && directionRef.current !== "giù") {
+      directionRef.current = "giù";
+      console.log(directionRef.current);
+    } else if (counter === initialValue && directionRef.current !== "") {
+      directionRef.current = "";
+      console.log(directionRef.current);
+    }
+
+    prevCounterRef.current = counter;
+  }, [counter, initialValue]);
+
+  function handleCounterIncrement() {
+    setCounter((count) => count + 1);
+  }
+
+  function handleCounterDecrease() {
+    setCounter((count) => count - 1);
+  }
+
+  function handleCounterReset() {
+    setCounter(initialValue);
+  }
+
+  return (
+    <div>
+      <CounterDisplay counter={counter} />
+      <Button onClick={handleCounterIncrement} label="Aumenta" />
+      <Button onClick={handleCounterDecrease} label="Diminuisci" />
+      <Button onClick={handleCounterReset} label="Resetta" />
+    </div>
+  );
+}
+
---
+++ refs/src/Login.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useRef } from "react";
+
+export function Login() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    session: false,
+  });
+
+  const mountedRef = useRef(false);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (!mountedRef.current) {
+      mountedRef.current = true;
+      console.log("Mounting for the first time");
+    } else {
+      console.log("Mounting again for debug purposes");
+    }
+
+    inputRef.current?.focus();
+  });
+
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
+
+    setData((data) => {
+      return {
+        ...data,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
+  function handleResetForm(event) {
+    event.preventDefault();
+    setData({
+      username: "",
+      password: "",
+      session: false,
+    });
+  }
+
+  return (
+    <div>
+      <form id="login-form">
+        <input
+          name="username"
+          placeholder="Username"
+          value={data.username}
+          onChange={handleInputChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={data.password}
+          onChange={handleInputChange}
+        />
+        <div id="remember-div">
+          <p>Remember me</p>
+          <input
+            id="checkbox"
+            name="session"
+            type="checkbox"
+            checked={data.session}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button onClick={handleResetForm}>Reset</button>
+      </form>
+    </div>
+  );
+}
